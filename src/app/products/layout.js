106c4345cfe308d2f45c98f8c47e27ce5d7e438d
a1shuvo <a1shuvosaha@gmail.com>
2025-08-21 21:14:42 +0600

    Added Footer in products and products details page

--- src/app/products/layout.js
+++ src/app/products/layout.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function ProductsLayout({ children }) {
@@ -5,6 +6,7 @@ export default function ProductsLayout({ children }) {
     <>
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </>
   );
 }
